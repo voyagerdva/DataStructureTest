d57cc917be90fb0f7ac4e81bb01b25169e42c21c
voyagerdva <mariner-9@yandex.ru>
2022-11-29 15:57:43 +0300

    test 1 almost finished

--- src/main/java/ru/zhuravl/Test.java
+++ src/main/java/ru/zhuravl/Test.java
@@ -1,5 +1,7 @@
 package ru.zhuravl;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
 //      TEST 1. Single add()
@@ -10,6 +12,11 @@ public class Test {
         String[] arr = fixt.arr;
 
         structList.add(arr, fixt.Item_SSS0);
+        System.out.println(Arrays.toString(arr));
+
+        if (Arrays.equals(arr, fixt.arrEthalon)) System.out.println("Тест пройден!");
+        else System.out.println("Тест НЕ пройден! :-((( ");
+
     }
 
 
