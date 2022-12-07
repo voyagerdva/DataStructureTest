32548d552fac45f05b2168860cb187a3c7927f26
voyagerdva <mariner-9@yandex.ru>
2022-12-07 16:05:27 +0300

    changed add() to addFront() method

--- src/main/java/ru/zhuravl/CheckFunctional.java
+++ src/main/java/ru/zhuravl/CheckFunctional.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CheckFunctional {
     public void checkAllMethods() {
-        System.out.println("\n\n========== Check add() method(): ");
+        System.out.println("\n\n========== Check addFront() method(): ");
         InterfaceList linkList1 = new LinkList();
         InterfaceList linkList2 = new LinkList();
 
@@ -12,13 +12,13 @@ public class CheckFunctional {
         String data2[] = {"22", "44", "77"};
 
         for (String item: data1) {
-            linkList1.add(item);
+            linkList1.addFront(item);
             addNodeAndPrint((LinkList) linkList1, item);
         }
         System.out.println(linkList1.getSize() + "\n-----------------------\n");
 
         for (String item: data2) {
-            linkList2.add(item);
+            linkList2.addFront(item);
             addNodeAndPrint((LinkList) linkList2, item);
         }
         System.out.println(linkList2.getSize() + "\n-----------------------\n");
@@ -44,11 +44,11 @@ public class CheckFunctional {
 //        String data1[] = {"11", "22", "33", "44", "55", "66"};
 //        String data2[] = {"44", "22", "X"};
 
-        for (String item: data1) linkList4.add(item);
+        for (String item: data1) linkList4.addFront(item);
         linkList4.printList();
         System.out.println("________________________ " + linkList4.getSize());
 
-        for (String item: data2) linkList5.add(item);
+        for (String item: data2) linkList5.addFront(item);
         linkList5.printList();
         System.out.println("________________________ " + linkList5.getSize());
 
@@ -80,7 +80,7 @@ public class CheckFunctional {
         InterfaceList linkList6 = new LinkList();
 
         for (String item: data1)
-            linkList6.add(item);
+            linkList6.addFront(item);
         linkList6.printList();
         System.out.println("\n" + linkList6.getSize() + "\n-----------------------\n");
 
--- src/main/java/ru/zhuravl/Test.java
+++ src/main/java/ru/zhuravl/Test.java
@@ -8,11 +8,11 @@ public class Test {
         InterfaceList linkList = new LinkList();
         String data = "WORD";
 
-        linkList.add(data);
+        linkList.addFront(data);
 
         String value = ((LinkList) linkList).getNode(0).getValue();
         String result = (value == fixture.test1_valueEthalon ? "+" : "-");
-        System.out.printf("%-6s %-15s %-1s\n", "Test 1", "ADD", result);
+        System.out.printf("%-6s %-15s %-1s\n", "Test 1", "addFront", result);
     }
 
     void test_2_size() {
@@ -21,7 +21,7 @@ public class Test {
 
         for (int i = 0; i < fixture.test3_qty; i++) {
             String data = "0x"+(Integer.toString(i,16)).toUpperCase();
-            linkList.add(data);
+            linkList.addFront(data);
         }
 
         String result = (linkList.getSize() == fixture.test3_qty ? "+" : "-");
@@ -33,8 +33,8 @@ public class Test {
         InterfaceList linkList1 = new LinkList();
         InterfaceList linkList2 = new LinkList();
 
-        for (String item : fixture.test4_data1) linkList1.add(item);
-        for (String item : fixture.test4_data2) linkList2.add(item);
+        for (String item : fixture.test4_data1) linkList1.addFront(item);
+        for (String item : fixture.test4_data2) linkList2.addFront(item);
 
         linkList1.addList((LinkList) linkList2);
 
@@ -47,8 +47,8 @@ public class Test {
         InterfaceList linkList1 = new LinkList();
         InterfaceList linkList2 = new LinkList();
 
-        for (String item : fixture.test5_data1) linkList1.add(item);
-        for (String item : fixture.test5_data2) linkList2.add(item);
+        for (String item : fixture.test5_data1) linkList1.addFront(item);
+        for (String item : fixture.test5_data2) linkList2.addFront(item);
 
         InterfaceList linkList3= linkList1.intersect((LinkList) linkList2);
         String result = (Arrays.equals(linkList3.getListValues(), fixture.test5_listEthalon) ? "+" : "-");
@@ -60,7 +60,7 @@ public class Test {
         Fixture fixture = new Fixture();
         InterfaceList linkList = new LinkList();
 
-        for (String item : fixture.test2_data) linkList.add(item);
+        for (String item : fixture.test2_data) linkList.addFront(item);
 
         linkList.removeHead();
         linkList.removeHead();
@@ -73,7 +73,7 @@ public class Test {
         Fixture fixture = new Fixture();
         InterfaceList linkList = new LinkList();
 
-        for (String item : fixture.test6_data) linkList.add(item);
+        for (String item : fixture.test6_data) linkList.addFront(item);
 
         linkList.removeElement(0);
         String result = (Arrays.equals(linkList.getListValues(), fixture.test6_listEthalon)
@@ -86,7 +86,7 @@ public class Test {
         Fixture fixture = new Fixture();
         InterfaceList linkList = new LinkList();
 
-        for (String item : fixture.test6_data) linkList.add(item);
+        for (String item : fixture.test6_data) linkList.addFront(item);
 
         linkList.removeElement(3);
         String result = (Arrays.equals(linkList.getListValues(), fixture.test7_listEthalon)
@@ -99,7 +99,7 @@ public class Test {
         Fixture fixture = new Fixture();
         InterfaceList linkList = new LinkList();
 
-        for (String item : fixture.test6_data) linkList.add(item);
+        for (String item : fixture.test6_data) linkList.addFront(item);
 
         linkList.removeElement(6);
         String result = (Arrays.equals(linkList.getListValues(), fixture.test8_listEthalon)
